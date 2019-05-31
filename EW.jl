270d06790fb54e1b54159d5bcfeae661e965a5a3
Menyoung Lee <menyounglee@gmail.com>
2019-05-31 16:48:22 -0400

    wait for user enter to start experiment

--- EW.jl
+++ EW.jl
@@ -2,13 +2,15 @@
 
 import NIDAQ, CSV, DataFrames, Dates
 
-export @init
+export init, arb
 
-function init(name, volt)
-    println("Disconnect (toggle off add) voltage.")
-    sleep(10)
+const dev = "Dev1"
+
+function init(volt)
+    println("Disconnect (toggle off add) voltage and press Enter")
+    readline()
     NIDAQ.ResetDevice(codeunits(name))
-    ao0 = NIDAQ.analog_output("$name/ao0")
+    ao0 = NIDAQ.analog_output("$dev/ao0")
     NIDAQ.start(ao0)
     NIDAQ.write(ao0,[volt])
     NIDAQ.clear(ao0)
@@ -17,20 +19,24 @@ end
 
 stamp = Dates.DateFormat("yymmdd_HHMMSS.c\\sv")
 
-function arb(name, period, volts)
-    ct = NIDAQ.generate_pulses("$name/ctr1", high = 240, low = period - 240)
+function arb(period, volts)
+    println("Set up video camera trigger and press Enter")
+    readline()
+    ct = NIDAQ.generate_pulses("$dev/ctr1", high = 240, low = period - 240)
     NIDAQ.CfgImplicitTiming(ct.th, NIDAQ.Val_FiniteSamps , length(volts))
-    ao = NIDAQ.analog_output("$name/ao0", range=[minimum(volts),maximum(volts)])
+    ao = NIDAQ.analog_output("$dev/ao0", range=[minimum(volts),maximum(volts)])
     NIDAQ.CfgSampClkTiming(ao.th, pointer("Ctr1InternalOutput"), 4000, NIDAQ.Val_Rising, NIDAQ.Val_FiniteSamps, length(volts))
     NIDAQ.write(ao, volts)
-    ai = NIDAQ.analog_input("$name/ai0:2")
+    ai = NIDAQ.analog_input("$dev/ai0:2")
     NIDAQ.CfgSampClkTiming(ai.th, pointer("Ctr1InternalOutput"), 4000, NIDAQ.Val_Rising, NIDAQ.Val_FiniteSamps, length(volts))
     NIDAQ.start(ai); NIDAQ.start(ao); NIDAQ.start(ct)
     sleep(1.01*period*(2+length(volts))./80e6) # clock is 80MHz
     Vi = NIDAQ.read(ai, Float64, length(volts))
     df = DataFrames.DataFrame(Vo = volts, V1 = Vi[:,1], Vx = Vi[:,2], Vy = Vi[:,3])
-    CSV.write(Dates.format(Dates.now(),stamp), df)
+    fn = Dates.format(Dates.now(),stamp)
+    CSV.write(fn, df)
     NIDAQ.clear(ai); NIDAQ.clear(ao); NIDAQ.clear(ct)
+    fn
 end
 
 #end
